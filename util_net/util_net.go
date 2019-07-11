409ad49ecbab992bde8daf725ed24ad1266d3daf
MrSep <mrsep18th@gmail.com>
2019-07-11 16:07:14 +0800

    add new package net

---
+++ util_net/util_net.go
@@ -0,0 +1,28 @@
+package util_net
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+func ParseQuery(r *http.Request) (url.Values, error) {
+	m, err := url.ParseQuery(r.URL.RawQuery)
+	return m, err
+}
+
+func ExecGet(urlstr string) ([]byte, error) {
+
+	resp, err := http.Get(urlstr)
+
+	var msg []byte
+
+	if err == nil {
+		defer resp.Body.Close()
+		msg, err = ioutil.ReadAll(resp.Body)
+	} else {
+
+	}
+
+	return msg, err
+}
