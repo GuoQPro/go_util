69b0b6482af94c28ae76e6770f3ce9be6b15bb03
MrSep <mrsep18th@gmail.com>
2019-08-08 14:36:05 +0800

    change function

--- util_net/util_net.go
+++ util_net/util_net.go
@@ -6,13 +6,23 @@ import (
 	"net/url"
 )
 
-func ParseQuery(r *http.Request) (url.Values, error) {
+func GetQueryValues(r *http.Request, keys ...interface{}) ([][]byte, error) {
 	m, err := url.ParseQuery(r.URL.RawQuery)
-	return m, err
+
+	if err != nil {
+		return nil, nil
+	}
+
+	var resultArray [][]byte
+
+	for i := 0; i < len(keys); i++ {
+		value := m.Get(keys[i].(string))
+		resultArray = append(resultArray, []byte(value))
+	}
+	return resultArray, err
 }
 
 func ExecGet(urlstr string) ([]byte, error) {
-
 	resp, err := http.Get(urlstr)
 
 	var msg []byte
